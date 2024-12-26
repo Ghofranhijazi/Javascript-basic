10c72e969380dad5ff575d86eb93b61ee51b09d3
Ghofranhijazi <ghofranhijazi02>
2024-12-26 16:20:54 +0300

    add

---
+++ Javascript.js
@@ -0,0 +1,43 @@
+            let x=4.6;
+            let y=3;
+                 
+console.log("Q1");
+console.log("1.)" + "JavaScript" +" "+ 50);
+console.log("2." + 17 % 5);
+console.log("3." + 5 % 17);
+console.log("4." + 5/10);
+console.log("5." +(4 === "4"));
+console.log("6." +(4 != 5));
+console.log("7." +(7 <= 8));
+console.log("8." + "Hello" +" "+ 5);
+console.log("9." +((Math.ceil(x)) - (Math.floor(x))));
+console.log("10." +(Math.pow(x,2)));
+
+console.log("Q2");
+let num=prompt("enter a number","0");
+window.alert(num);
+
+console.log("Q3");
+let n1=parseInt(prompt("enter the first number","0"));
+let n2=parseInt(prompt("enter the second number","0"));
+window.alert(" the sum of numbers"+" "+ (n1+n2));
+
+console.log("Q4");
+let n3=parseInt(prompt("enter the first number","0"));
+let n4=parseInt(prompt("enter the second number","0"));
+window.alert(" the product of numbers"+" "+ (n3*n4));
+
+console.log("Q5");
+let n5=parseInt(prompt("enter the first number","0"));
+let n6=parseInt(prompt("enter the second number","0"));
+window.alert(" the dividing of numbers"+" "+ (n5/n6));
+
+console.log("Q6");
+window.alert(5+3);
+
+
+
+
+
+
+
